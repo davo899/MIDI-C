864619f66ae2ac55391c52a1cb450c2b9436e659
djs21 <djs21@ic.ac.uk>
2023-06-09 23:51:10 +0100

    Implemented read_file_to_byte_array function

--- src/main/MIDI.c
+++ src/main/MIDI.c
@@ -1,7 +1,17 @@
 #include "MIDI.h"
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 uint8_t* read_file_to_byte_array(char* filename, int* length) {
-  *length = 0;
-  return NULL;
+  FILE *fileptr = fopen(filename, "rb");
+  fseek(fileptr, 0, SEEK_END);
+  *length = ftell(fileptr);
+  rewind(fileptr);
+
+  uint8_t *buffer = (uint8_t *) malloc(*length * sizeof(uint8_t));
+  fread(buffer, *length, 1, fileptr);
+  fclose(fileptr);
+  
+  return buffer;
 }

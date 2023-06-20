c8acaa4ee49c1773e643969f54de9ca3a99e139b
David Sadler <djs21@ic.ac.uk>
2023-06-20 17:03:41 +0100

    Added fail count to test runner

--- Makefile
+++ Makefile
@@ -5,15 +5,15 @@ SRC = $(wildcard src/main/*.c) $(wildcard src/test/*.c)
 DEPS = $(wildcard src/main/*.h) $(wildcard src/test/*.h)
 OBJ = $(patsubst src/%.c,$(OBJ_DIR)/%.o,$(SRC))
 
+test: $(OBJ)
+	$(CC) -o $@ $^
+
 dirs:
 	mkdir -p $(OBJ_DIR)/main
 	mkdir -p $(OBJ_DIR)/test
 
 $(OBJ_DIR)/%.o: src/%.c $(DEPS) dirs
 	$(CC) -c -o $@ $<
-
-test: $(OBJ)
-	$(CC) -o $@ $^
 
 .PHONY: clean
 
--- src/test/test.c
+++ src/test/test.c
@@ -30,6 +30,8 @@ struct test tests[] = {
 int tests_length = sizeof(tests) / sizeof(struct test);
 
 int main() {
+  int fail_count = 0;
+
   printf("\nRunning tests...\n");
   for (int i = 0; i < tests_length; i++) {
     printf("\033[0;37m");
@@ -40,8 +42,15 @@ int main() {
     } else {
       printf("\033[0;31m");
       printf("failed");
+      fail_count++;
     }
     printf("\n");
+  }
+  if (fail_count > 0) {
+    printf("\033[0m");
+    printf("\n%d tests ", fail_count);
+    printf("\033[0;31m");
+    printf("failed");
   }
   printf("\033[0m");
 }

16ad3b66765b149782cbd08fda4d70fd7a8d0517
David Sadler <djs21@ic.ac.uk>
2023-06-22 13:55:45 +0100

    Added next_track_event function

--- src/main/event.c
+++ src/main/event.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct event* next_track_event(struct MIDI_file* MIDI_file, uint8_t event_code) {
+  return unimplemented_event_reader(MIDI_file, event_code);
+}
+
 struct event* unimplemented_event_reader(struct MIDI_file* MIDI_file, uint8_t event_code) {
   printf("Error: encountered unknown event code 0x%x", event_code);
   exit(1);

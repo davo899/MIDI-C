91394ee2300392c4ee578ca4ec0fba0748f45fda
David Sadler <djs21@ic.ac.uk>
2023-06-23 23:22:28 +0100

    Added copyright notice event reader

--- src/test/meta_event_test.c
+++ src/test/meta_event_test.c
@@ -26,9 +26,24 @@ static int reads_text() {
   return TEST_PASS;
 }
 
+static int reads_copyright_notice() {
+  uint8_t bytes[] = { 0x7C, 0xFF, 0x02, 0x02, 0x68, 0x69 };
+  INIT_MIDI_FILE(MIDI_file);
+  struct event* event = next_track_event(&MIDI_file);
+  ASSERT(event != NULL);
+  ASSERT(event->deltatime == 0x7C);
+  ASSERT(event->type == COPYRIGHT_NOTICE);
+  struct text_buffer* text_buffer = (struct text_buffer*)event->body;
+  ASSERT(text_buffer->length == 2);
+  ASSERT(text_buffer->text[0] == 'h');
+  ASSERT(text_buffer->text[1] == 'i');
+  return TEST_PASS;
+}
+
 static struct test tests[] = {
   { .name = "Reads sequence number event", .function = &reads_sequence_number },
   { .name = "Reads text event", .function = &reads_text },
+  { .name = "Reads copyright notice event", .function = &reads_copyright_notice },
 };
 
 INIT_TEST_GROUP(meta_event);

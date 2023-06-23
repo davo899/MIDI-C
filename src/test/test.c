f0d818d69f6ef9c832b78f6a286f0fe4f66a11dd
David Sadler <djs21@ic.ac.uk>
2023-06-23 14:22:40 +0100

    Refactored colour prints to improve readability

--- src/test/test.c
+++ src/test/test.c
@@ -2,39 +2,54 @@
 #include "tests.h"
 #include <stddef.h>
 
+enum colour {
+  RESET, RED, GREEN, WHITE
+};
+
 struct test_group *test_groups[] = {
   &MIDI_tests,
   &MIDI_event_tests
 };
 int test_groups_length = sizeof(test_groups) / sizeof(struct test_group*);
 
+static void set_colour(enum colour colour) {
+  switch (colour) {
+    case RESET: printf("\033[0m");    break;
+    case RED:   printf("\033[0;31m"); break;
+    case GREEN: printf("\033[0;32m"); break;
+    case WHITE: printf("\033[0;37m"); break;
+  };
+}
+
 int main() {
   int fail_count = 0;
 
   printf("\nRunning tests...\n");
   for (int i = 0; i < test_groups_length; i++) {
-    printf("\033[0;37m");
     printf("%s\n", test_groups[i]->name);
+
     for (int j = 0; j < test_groups[i]->size; j++) {
       struct test test = test_groups[i]->tests[j];
-      printf("\033[0;37m");
+
       printf("\t%s: ", test.name);
       if (test.function() == TEST_PASS) {
-        printf("\033[0;32m");
+        set_colour(GREEN);
         printf("passed");
       } else {
-        printf("\033[0;31m");
+        set_colour(RED);
         printf("failed");
         fail_count++;
       }
       printf("\n");
+      set_colour(WHITE);
     }
+    printf("\n");
   }
   if (fail_count > 0) {
-    printf("\033[0m");
+    set_colour(RESET);
     printf("\n%d tests ", fail_count);
-    printf("\033[0;31m");
+    set_colour(RED);
     printf("failed");
   }
-  printf("\033[0m");
+  set_colour(RESET);
 }

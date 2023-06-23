42ddd6e1b051d13f3a9653e28f9c9f96ecb293d7
David Sadler <djs21@ic.ac.uk>
2023-06-23 19:43:54 +0100

    Added tests for note on and polyphonic key pressure events

--- src/test/MIDI_event_test.c
+++ src/test/MIDI_event_test.c
@@ -15,8 +15,38 @@ static int reads_note_off() {
   return TEST_PASS;
 }
 
+static int reads_note_on() {
+  uint8_t bytes[] = { 0x03, 0x9A, 0x7F, 0x3A };
+  INIT_MIDI_FILE(MIDI_file);
+  struct event* event = next_track_event(&MIDI_file);
+  ASSERT(event->deltatime == 0x03);
+  ASSERT(event->type == NOTE_ON);
+  struct MIDI_event* MIDI_event = (struct MIDI_event*)event->body;
+  ASSERT(MIDI_event->channel == 0xA);
+  struct note_toggle* note_toggle = (struct note_toggle*)MIDI_event->body;
+  ASSERT(note_toggle->key == 0x7F);
+  ASSERT(note_toggle->velocity == 0x3A);
+  return TEST_PASS;
+}
+
+static int reads_polyphonic_key_pressure() {
+  uint8_t bytes[] = { 0x03, 0xAA, 0x7F, 0x3A };
+  INIT_MIDI_FILE(MIDI_file);
+  struct event* event = next_track_event(&MIDI_file);
+  ASSERT(event->deltatime == 0x03);
+  ASSERT(event->type == POLYPHONIC_KEY_PRESSURE);
+  struct MIDI_event* MIDI_event = (struct MIDI_event*)event->body;
+  ASSERT(MIDI_event->channel == 0xA);
+  struct note_toggle* note_toggle = (struct note_toggle*)MIDI_event->body;
+  ASSERT(note_toggle->key == 0x7F);
+  ASSERT(note_toggle->velocity == 0x3A);
+  return TEST_PASS;
+}
+
 static struct test tests[] = {
-  { .name = "Reads note off event", .function = &reads_note_off }
+  { .name = "Reads note off event", .function = &reads_note_off },
+  { .name = "Reads note on event", .function = &reads_note_on },
+  { .name = "Reads polyphonic key pressure event", .function = &reads_polyphonic_key_pressure },
 };
 
 INIT_TEST_GROUP(MIDI_event);
